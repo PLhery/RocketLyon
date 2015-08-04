8b17cd9f20697e944b624128c7ab59f34a8e97e2
Mehdi Baha <elmehdi.baha@gmail.com>
2015-08-04 19:57:21 +0100

    Reformat stations.json to work better with Gson

--- RocketLyonAndroid/app/src/main/java/com/insa/rocketlyonandroid/models/ApiResponse.java
+++ RocketLyonAndroid/app/src/main/java/com/insa/rocketlyonandroid/models/ApiResponse.java
@@ -1,6 +1,15 @@
 package com.insa.rocketlyonandroid.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 
 public class ApiResponse extends ArrayList<Arret> {
+
+    @SerializedName("arrets")
+    private ArrayList<Arret> arrets;
+
+    public ApiResponse() {
+
+    }
 }
--- RocketLyonAndroid/app/src/main/java/com/insa/rocketlyonandroid/models/Ligne.java
+++ RocketLyonAndroid/app/src/main/java/com/insa/rocketlyonandroid/models/Ligne.java
@@ -47,6 +47,10 @@ public class Ligne {
         return l;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
     public void setNom(String nom) {
         this.nom = nom;
     }
